85f1501e5c1fcd999dfbc440c9e3d0c84d8901ef
Zinoviy Kuzmitch <93143550+DedZinoviy@users.noreply.github.com>
2022-03-14 22:56:51 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+def negativeProbability(probability):
+    return 1 - probability
+
+machinesNumber = input()
+machilneProbabilityList = list.append(input())
+
+def atLeastOneMachineBreak(machinesNumber, machilneProbabilityList):
+    for i in range(machinesNumber): #{Хотя бы один сломается} = {Не ни один не сломается} 
+        resultProbability *= negativeProbability(machilneProbabilityList[i])
+    return resultProbability
+
+def allMachinesBreake(machinesNumber, machilneProbabilityList):
+    for i in range(machinesNumber): # {Все станки сломаются} 
+        resultProbability *= machilneProbabilityList[i]
+    return resultProbability
+
+def onlyChosenMachine(machineNumber, machineProbabilityLeast):
+    reusltProbability = machineProbabilityLeast[machineNumber] # {Только конкретный станок выйдет из строя}
+    return reusltProbability
