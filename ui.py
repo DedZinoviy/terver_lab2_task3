f3054d5792e635f6cb351ba9bed54b5acd37d1bb
DmitryDemenkov <demon.krut2002@mail.ru>
2022-04-18 17:06:06 +0300

    new formules

--- ui.py
+++ ui.py
@@ -14,7 +14,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(904, 409)
+        MainWindow.resize(904, 449)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
